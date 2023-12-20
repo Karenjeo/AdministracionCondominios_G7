2bc9c46d6985a1ec8c26dde2ed2a02832cd2821e
sebas17sf <84949041+sebas17sf@users.noreply.github.com>
2023-12-20 10:08:30 -0500

    validaciones unidades

--- src/app/unidades/unidades.component.ts
+++ src/app/unidades/unidades.component.ts
@@ -68,6 +68,25 @@ export class UnidadesComponent implements OnInit {
   }
   
 
-  
+    validarCampos(){
+      var nombre = (<HTMLInputElement>document.getElementById("nombre")).value;
+      var apellido = (<HTMLInputElement>document.getElementById("apellido")).value;
+      var direccion = (<HTMLInputElement>document.getElementById("direccion")).value;
+      var ciudad = (<HTMLInputElement>document.getElementById("ciudad")).value;
+      var estado = (<HTMLInputElement>document.getElementById("estado")).value;
+      var codigo = (<HTMLInputElement>document.getElementById("codigo")).value;
+      var telefono = (<HTMLInputElement>document.getElementById("telefono")).value;
+      var email = (<HTMLInputElement>document.getElementById("email")).value;
+      var tarjeta = (<HTMLInputElement>document.getElementById("tarjeta")).value;
+      var nombreTarjeta = (<HTMLInputElement>document.getElementById("nombreTarjeta")).value;
+      var fecha = (<HTMLInputElement>document.getElementById("fecha")).value;
+      var cvv = (<HTMLInputElement>document.getElementById("cvv")).value;
+    
+      if(nombre == "" || apellido == "" || direccion == "" || ciudad == "" || estado == "" || codigo == "" || telefono == "" || email == "" || tarjeta == "" || nombreTarjeta == "" || fecha == "" || cvv == ""){
+        alert("Todos los campos son obligatorios");
+      }else{
+        alert("Compra realizada con exito");
+      }
+    }
 
 }
