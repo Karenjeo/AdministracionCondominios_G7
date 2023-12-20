022b6041e1af0eaeb54d8b78bc6dd4168f9b71f7
sebas17sf <84949041+sebas17sf@users.noreply.github.com>
2023-12-20 10:06:18 -0500

    correcion d validaciones

--- src/app/pago/pago.component.ts
+++ src/app/pago/pago.component.ts
@@ -12,4 +12,20 @@ export class PagoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+   validarCampos(){
+    var nombre = (<HTMLInputElement>document.getElementById("nombre")).value;
+    var apellido = (<HTMLInputElement>document.getElementById("apellido")).value;
+    var direccion = (<HTMLInputElement>document.getElementById("direccion")).value;
+    var ciudad = (<HTMLInputElement>document.getElementById("ciudad")).value;
+    var estado = (<HTMLInputElement>document.getElementById("estado")).value;
+    var codigo = (<HTMLInputElement>document.getElementById("codigo")).value;
+    var telefono = (<HTMLInputElement>document.getElementById("telefono")).value;
+ 
+
+    if(nombre == "" || apellido == "" || direccion == "" || ciudad == "" || estado == "" || codigo == "" || telefono == "" || email == "" || tarjeta == "" || nombreTarjeta == "" || fecha == "" || cvv == ""){
+      alert("Todos los campos son obligatorios");
+    }else{
+      alert("Compra realizada con exito");
+    }
+  }
 }
