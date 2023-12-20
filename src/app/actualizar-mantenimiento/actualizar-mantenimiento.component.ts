333a79e351a423c13822c7acba127817cc4f5594
sebas17sf <84949041+sebas17sf@users.noreply.github.com>
2023-12-20 10:10:50 -0500

    correcion de validaminentos

--- src/app/actualizar-mantenimiento/actualizar-mantenimiento.component.ts
+++ src/app/actualizar-mantenimiento/actualizar-mantenimiento.component.ts
@@ -11,5 +11,18 @@ export class ActualizarMantenimientoComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  validarCampos(){
+    var nombre = (<HTMLInputElement>document.getElementById("nombre")).value;
+    var apellido = (<HTMLInputElement>document.getElementById("apellido")).value;
+    var direccion = (<HTMLInputElement>document.getElementById("direccion")).value;
+    var ciudad = (<HTMLInputElement>document.getElementById("ciudad")).value;
+    var estado = (<HTMLInputElement>document.getElementById("estado")).value;
+    var codigo = (<HTMLInputElement>document.getElementById("codigo")).value;
+     
+    if(nombre == "" || apellido == "" || direccion == "" || ciudad == "" || estado == "" || codigo == "" || telefono == "" || email == "" || tarjeta == "" || nombreTarjeta == "" || fecha == "" || cvv == ""){
+      alert("Todos los campos son obligatorios");
+    }else{
+      alert("Compra realizada con exito");
+    }
+  }
 }
